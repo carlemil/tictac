f979770a4b7594f1117434be00a3ac4108ea59bb
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-12-05 15:23:08 +0100

    in the middle

--- src/se/kjellstrand/mcts1/MCTS1.java
+++ src/se/kjellstrand/mcts1/MCTS1.java
@@ -10,20 +10,51 @@ public class MCTS1 {
 	private int[][] winMovesBoard = new int[3][3];
 
 	public GameState makeNextMove(TicTac tt) {
+		GameState gs = null;
+		int possibleMoveIndex = -1;
 		long time = System.currentTimeMillis();
-		// iterate for x times to find a good move
-		while (time + 100 < System.currentTimeMillis()) {
+		while (time + 100 > System.currentTimeMillis()) {
 			TicTac ttClone = tt.clone();
 			int possibleMoves = ttClone.getNumberOfPossibleMoves();
-			int possibleMoveIndex = (int) (Math.random() * possibleMoves);
+			possibleMoveIndex = (int) (Math.random() * possibleMoves);
 			Position pos = ttClone.getPosForIndex(possibleMoveIndex);
 			madeMovesBoard[pos.x][pos.y]++;
-			ttClone.makeMove(possibleMoveIndex);
+			gs = ttClone.makeMove(possibleMoveIndex);
+
+			// playout random play
+			while (gs == GameState.P1S_TURN || gs == GameState.P2S_TURN) {
+				possibleMoves = ttClone.getNumberOfPossibleMoves();
+				int nextMoveIndex = (int) (Math.random() * possibleMoves);
+				gs = ttClone.makeMove(nextMoveIndex);
+			}
+			if (gs == GameState.P1_WINS) {
+				winMovesBoard[pos.x][pos.y]++;
+			}
 		}
 
-		// make the best move
+		// print result board
+		System.out.println("total tries");
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				System.out.print(madeMovesBoard[x][y] + " ");
+			}
+			System.out.println("");
+		}
 
-		return null;
+		// print result board
+		System.out.println("total wins");
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				System.out.print(winMovesBoard[x][y] + " ");
+			}
+			System.out.println("");
+		}
+
+		// select best move and make it
+
+		gs = tt.makeMove(possibleMoveIndex);
+
+		return gs;
 	}
 
 }
