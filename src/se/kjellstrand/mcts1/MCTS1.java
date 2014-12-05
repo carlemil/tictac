603b38ec66c1d7652a0b19f549b717142a208410
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-12-05 15:23:08 +0100

    in the middle

--- src/se/kjellstrand/mcts1/MCTS1.java
+++ src/se/kjellstrand/mcts1/MCTS1.java
@@ -10,20 +10,53 @@ public class MCTS1 {
 	private int[][] winMovesBoard = new int[3][3];
 
 	public GameState makeNextMove(TicTac tt) {
+		GameState gs = null;
+		int possibleMoveIndex = -1;
 		long time = System.currentTimeMillis();
-		// iterate for x times to find a good move
-		while (time + 100 < System.currentTimeMillis()) {
+		while (time + 100 > System.currentTimeMillis()) {
 			TicTac ttClone = tt.clone();
 			int possibleMoves = ttClone.getNumberOfPossibleMoves();
-			int possibleMoveIndex = (int) (Math.random() * possibleMoves);
+			possibleMoveIndex = (int) (Math.random() * possibleMoves);
 			Position pos = ttClone.getPosForIndex(possibleMoveIndex);
 			madeMovesBoard[pos.x][pos.y]++;
-			ttClone.makeMove(possibleMoveIndex);
+			gs = ttClone.makeMove(possibleMoveIndex);
+
+			// playout random play
+			while (gs == GameState.P1S_TURN || gs == GameState.P2S_TURN) {
+				//System.out.println("gs "+gs);
+				possibleMoves = ttClone.getNumberOfPossibleMoves();
+				int nextMoveIndex = (int) (Math.random() * possibleMoves);
+				gs = ttClone.makeMove(nextMoveIndex);
+			}
+			//System.out.println("gs "+gs);
+			if (gs == GameState.P1_WINS) {
+				winMovesBoard[pos.x][pos.y]++;
+			}
 		}
 
-		// make the best move
+		// print result board
+		System.out.println("total tries");
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				System.out.print(madeMovesBoard[x][y] + " ");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+		// print result board
+		System.out.println("total wins");
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				System.out.print(winMovesBoard[x][y] + " ");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+		// select best move and make it
 
-		return null;
+		gs = tt.makeMove(possibleMoveIndex);
+
+		return gs;
 	}
 
 }
--- src/se/kjellstrand/tictac/Main.java
+++ src/se/kjellstrand/tictac/Main.java
@@ -4,14 +4,11 @@ import se.kjellstrand.mcts1.MCTS1;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		MCTS1 mcts1 = new MCTS1();
 		TicTac tt = new TicTac();
 		GameState gs;
-		for(int i=0;i<2;i++){
+		for(int i=0;i<5;i++){
 			int possibleMoves = tt.getNumberOfPossibleMoves();
 			gs = tt.makeMove((int)(Math.random()*possibleMoves));
 			tt.printBoard();
@@ -19,8 +16,8 @@ public class Main {
 		}
 		System.out.println("evaluate next move");
 		gs = mcts1.makeNextMove(tt);
-		tt.printBoard();
-		System.out.println("state: "+gs.toString());
+		//tt.printBoard();
+		//System.out.println("state: "+gs.toString());
 //		TicTac tt2 = tt.clone();
 //		
 //		tt2.printBoard();
